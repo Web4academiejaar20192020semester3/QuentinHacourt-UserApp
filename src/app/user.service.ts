316ee03222698fecc53dddc3c5578a7cfeea165a
QuentinHacourt <quentin.hacourt@student.ucll.be>
2020-08-16 00:06:09 +0200

    post request wont work

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private getUsersUrl = 'http://localhost:8080/ChatApp_war_exploded/Controller?action=GetUsers';
+  private postUserUrl = 'http://localhost:8080/ChatApp_war_exploded/Controller?action=UpdateUser'
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.getUsersUrl)
+    return this.http.get<User[]>(this.getUsersUrl);
   }
 
   updateUser(user: User) {
-    const postUrl = 'http://localhost:8080/ChatApp_war_exploded/Controller?action=EditUser&voornaam=' + user.voornaam + '&naam=' + user.naam + '&age=' + user.leeftijd + '&geslacht=' + user.geslacht + '&email=' + user.email + '&userStatus=' + user.userStatus;
-    this.http.post(postUrl, null );
+    alert(user.voornaam);
+    this.http.post(this.postUserUrl, null, this.httpOptions).pipe(
+      catchError(this.handleError<any>('updateUser'))
+    )
   }
 
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); // log to console instead
+
+      alert(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,8 +1,8 @@
-export class User {
-  naam: string;
-  voornaam: string;
-  email: string;
-  geslacht: string;
-  leeftijd: string;
-  userStatus: string;
+export interface User {
+  naam: string,
+  voornaam: string,
+  email: string,
+  geslacht: string,
+  leeftijd: string,
+  userStatus: string
 }
